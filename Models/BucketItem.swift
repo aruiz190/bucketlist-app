2013ecaee34fe30c1805455ab9ed4adb49365457
aruiz190 <andrew.ruiz215@gmail.com>
2025-06-04 03:01:09 -0700

    Update BucketItem.swift

--- Models/BucketItem.swift
+++ Models/BucketItem.swift
@@ -1,1 +1,11 @@
+import Foundation
+
+struct BucketItem: Identifiable, Codable {
+    var id: String?
+    var title: String
+    var imageURL: String?
+    var createdBy: String
+    var listID: String
+    var timestamp: Date
+}
 
